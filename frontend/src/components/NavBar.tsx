3a66e55fe80564b2015329b51dceb10ff5dd7212
nagosu <nozick1021@gmail.com>
2023-09-25 19:42:55 +0900

    design : navigation bar 반응형 구현

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,21 +3,25 @@ import magnifier from '../assets/images/svg/magnifier.svg';
 
 function NavBar() {
   return (
-    <div className="flex justify-between items-center w-screen h-16 border-b-[1px] border-solid border-[#C6C6C6] bg-white">
+    <div className="flex justify-between items-center w-screen h-14 md:h-16 border-b-[1px] border-solid border-[#C6C6C6] bg-white">
       {/* 로고 */}
       <button
-        className="basis-1/4 text-left text-2xl font-LexendDeca font-bold ml-5"
+        className="flex basis-1/4 text-left text-xl md:text-2xl font-LexendDeca font-bold ml-5"
         type="button"
       >
         <span className="text-[#06439F]">Spotlight</span>
         <span className="text-[#FFD600]">Seoul</span>
       </button>
       {/* 검색창 */}
-      <div className="flex basis-2/4 w-[36rem] h-10 rounded-xl border-[1px] border-solid border-[#C6C6C6] bg-[#F9F9F9]">
-        <img className="w-5 ml-2" src={magnifier} alt="magnifier" />
-        <input className="bg-[#F9F9F9] ml-1" type="text" placeholder="search" />
+      <div className="flex basis-2/4 w-11/12 h-7 sm:h-9 md:h-11 rounded-xl border-[1px] border-solid border-[#C6C6C6] bg-[#F9F9F9]">
+        <img className="w-4 md:w-5 ml-2" src={magnifier} alt="magnifier" />
+        <input
+          className="bg-[#F9F9F9] ml-1 text-sm md:text-base"
+          type="text"
+          placeholder="search"
+        />
       </div>
-      <div className="basis-1/4" />
+      <div className="flex basis-1/4" />
     </div>
   );
 }
