d8169734f169669cb27af8cf8941ede8f443daa3
nagosu <nozick1021@gmail.com>
2023-10-05 01:35:12 +0900

    feat : 네이버지도 API 동작 확인

--- frontend/src/components/MapInformation.tsx
+++ frontend/src/components/MapInformation.tsx
@@ -1,20 +1,71 @@
-import MapSampleImage from '../assets/images/png/MapSampleImage.png';
+import { useEffect } from 'react';
+
+let mapInstance: naver.maps.Map | null = null;
+
+const loadScript = (src: string, callback: () => void) => {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = src;
+  script.onload = () => callback();
+  document.head.appendChild(script);
+};
 
 function MapInformation() {
+  const lat = 37.6037552097988;
+  const lot = 127.022203675321;
+
+  const initMap = () => {
+    // 추가 옵션 설정
+    const mapOptions = {
+      zoomControl: true,
+      zoomControlOptions: {
+        style: naver.maps.ZoomControlStyle.SMALL,
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+      center: new naver.maps.LatLng(lat, lot),
+      zoom: 16,
+    };
+
+    // 지도 초기화 확인
+    if (!mapInstance) {
+      mapInstance = new naver.maps.Map('map', mapOptions);
+    }
+
+    // Marker 생성
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(lat, lot),
+      map: mapInstance,
+    });
+
+    // 코드 수정 부분: 마커 클릭 이벤트
+    naver.maps.Event.addListener(marker, 'click', () => {
+      mapInstance?.setCenter(new naver.maps.LatLng(lat, lot));
+      mapInstance?.setZoom(16);
+    });
+  };
+
+  useEffect(() => {
+    // 스크립트 로딩 확인
+    if (typeof naver === 'undefined') {
+      loadScript(
+        'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=2nezq8tgn5',
+        initMap,
+      );
+    } else {
+      initMap();
+    }
+  }, []);
+
   return (
-    <div>
+    <>
       {/* 위치 정보(지도) */}
-      <div className="mb-8 mt-40 flex flex-col items-center">
+      <div className="mb-8 mt-40 flex w-screen flex-col items-center">
         <span className="sm:text-md font-LexendDeca text-sm font-bold text-[#06439F] md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           위치 안내
         </span>
-        <img
-          src={MapSampleImage}
-          alt="MapSampleImage"
-          className="mt-4 h-auto w-11/12 sm:mt-6 lg:mt-8"
-        />
+        <div id="map" className="mt-4 h-[500px] w-11/12 sm:mt-6 lg:mt-8" />
       </div>
-    </div>
+    </>
   );
 }
 
