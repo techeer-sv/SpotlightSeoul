cf9a20ce926c8affe19ef95810cc20f5bcae1601
nagosu <nozick1021@gmail.com>
2023-10-27 02:07:18 +0900

    feat : 네비게이션 바 api 연결중

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,64 @@
 import '../index.css';
 import magnifier from '../assets/images/svg/magnifier.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type PostResponse = {
+  id: number;
+  org_name: string;
+  main_img: string;
+  strt_date: string;
+  end_date: string;
+  title: string;
+  major_code_name: string;
+  sub_code_name: string;
+  date: string;
+};
+
+type searchResponseData = {
+  total_page_num: number;
+  post_responses: PostResponse[];
+};
 
 function NavBar() {
+  const [searchTitle, setSearchTitle] = useState<string>('');
+  const [postId, setPostID] = useState<number>(0);
+  const [orgName, setOrgName] = useState<string>('');
+  const [mainImg, setMainImg] = useState<string>('');
+  const [strtDate, setStrtDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [majorCodeName, setMajorCodeName] = useState<string>('');
+  const [subCodeName, setSubCodeName] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+
+  const inputSearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchTitle(e.target.value);
+  };
+
+  const fetchSearchResults = async () => {
+    try {
+      const response = await axios.post<searchResponseData>(
+        'http://localhost:8080/api/v1/festivals',
+        { title: searchTitle },
+      );
+      const searchResponseData: searchResponseData = response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [searchTitle]);
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      fetchSearchResults();
+    }
+  };
+
   return (
     <div className="flex h-14 w-screen items-center justify-between border-b-[1px] border-solid border-[#C6C6C6] bg-white md:h-16">
       {/* 로고 */}
@@ -19,6 +76,8 @@ function NavBar() {
           className="ml-1 bg-[#F9F9F9] text-sm md:text-base"
           type="text"
           placeholder="search"
+          onChange={inputSearchHandler}
+          onKeyDown={handleKeyDown}
         />
       </div>
       <div className="flex basis-1/4" />
