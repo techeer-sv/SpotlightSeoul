8fc748255bf8063ba1fc0905b77cbf255a64f5b2
“mineii” <kmaoo0406@naver.com>
2023-10-27 15:03:23 +0900

    feat: 카테고리별 색상 분류

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -19,6 +19,19 @@ function PostCard({
 }) {
   const navigate = useNavigate();
   // console.log('postid', postId);
+
+  const backgroundColor =
+    category === '공연'
+      ? '#E34646'
+      : category === '전시'
+      ? '#4A6BAB'
+      : category === '축제'
+      ? '#88D64C'
+      : category === '교육/체험'
+      ? '#70D4A4'
+      : category === '기타'
+      ? '#AE5D97'
+      : '#000000'; // 기본값 (여기서는 검은색) 설정
   return (
     <div
       onClick={() => {
@@ -33,7 +46,7 @@ function PostCard({
         {/* 썸네일 이미지 */}
         <div className="">
           <img
-            className="mx-auto w-[140px] rounded-[10px] "
+            className="mx-auto h-[198px] w-[140px] rounded-[10px] "
             src={mainImg}
             alt="thum"
           />
@@ -42,8 +55,9 @@ function PostCard({
             w-[60px] items-center justify-center
             rounded-bl-[10px] rounded-tr-[10px] bg-[#E34646] text-center font-LexendDeca text-xs
             font-medium text-white "
+            style={{ backgroundColor }}
           >
-            공연
+            {category}
           </div>
         </div>
 
